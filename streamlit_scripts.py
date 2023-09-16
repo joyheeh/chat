c765ca459f9df5eede71bd3fc4df9d05519a0cef
Jinhee <75326467+joyheeh@users.noreply.github.com>
2023-09-16 18:34:19 +0900

    0915files

--- streamlit_scripts.py
+++ streamlit_scripts.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 # from streamlit.components.v1 import html
 
 
-in_out_df = pd.read_csv("preprocessed_data/" +  "in_out_2023-09-15-10-48-46.csv")
-my_katalk_df = pd.read_csv("preprocessed_data/" +  "kakao_msg_2023-09-15-10-48-46.csv")
+in_out_df = pd.read_csv("preprocessed_data/" +  "in_out_2023-09-16-18-20-35.csv")
+my_katalk_df = pd.read_csv("preprocessed_data/" +  "kakao_msg_2023-09-16-18-20-35.csv")
 
 st.info('로마드 :red[2주 챌린지] 현황 보드 확인해보세요! https://roalnamchallenge1.streamlit.app', icon="📢")
 
